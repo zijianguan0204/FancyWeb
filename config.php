b916b259e5aaab004ad3cf2a66a530205a6f0350
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2019-12-22 22:33:25 -0800

    Finish connecting with DB

--- config.php
+++ config.php
@@ -1,8 +1,4 @@
 <?php
-    // Reference: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-
-    /* Database credentials. Assuming you are running MySQL
-    server with default setting (user 'root' with no password) */
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
--- index.php
+++ index.php
@@ -18,25 +18,31 @@
   <?php
 
   if(isset($_POST['signup'])){
-    if(isset($_POST['firstname'])){
-      $firstname=$_POST['firstname'];
-    }
-    if(isset($_POST['lastname'])){
-      $lastname=$_POST['lastname'];
-    }
+
     if(isset($_POST['username'])){
       $username=$_POST['username'];
     }
     if(isset($_POST['password'])){
       $password=$_POST['password'];
     }
-    if(isset($_POST['email'])){
-      $email=$_POST['email'];
-    }
     if(isset($_POST['phone'])){
       $phone=$_POST['phone'];
     }
-    
+    if(isset($_POST['email'])){
+      $email=$_POST['email'];
+    }
+    if(isset($_POST['firstname'])){
+      $firstname=$_POST['firstname'];
+    }
+    if(isset($_POST['lastname'])){
+      $lastname=$_POST['lastname'];
+    }
+
+    $sql = "INSERT INTO user_account (username, password, phone, email, firstname, lastname) VALUES('$username', '$password', '$phone', '$email', '$firstname', '$lastname')";
+    $stmt = mysqli_prepare($db_connection, $sql) or die(mysqli_error($db_connection));
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    mysqli_close($db_connection);
   }
 
   ?>
