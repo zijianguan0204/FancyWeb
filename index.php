0398c3fbe02b2f03e2ac5aaecd6c8476623b6f17
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2019-12-24 21:44:30 -0800

    Login Success, First Version

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ require_once('config.php');
     $stmt = mysqli_prepare($db_connection, $sql) or die(mysqli_error($db_connection));
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
-    mysqli_close($db_connection);
+    //mysqli_close($db_connection);
   }
 
   ?>
@@ -75,6 +75,9 @@ require_once('config.php');
         </li>
         <li class="nav-item">
           <button type="button" class="btn btn-link" data-toggle="modal" data-target="#signup">Sign up</button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-link" data-toggle="modal" data-target="#login">Log in</button>
         </li>
       </ul>
     </div>
@@ -179,11 +182,75 @@ require_once('config.php');
     </div>
   </div>
 
+  <!-- Log in feature: -->
+  <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <form action = "index.php" method="post">
+          <div class="modal-header">
+            <h5 class="modal-title" id="login">Log In</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class = "container">
+              <div class="form-group">
+                <label for="user_name" class="col-form-label">UserName:</label>
+                <input type="text" class="form-control" id="user_name" required>
+              </div>
+              <div class="form-group">
+                <label for="pass_word" class="col-form-label">Password:</label>
+                <input type="text" class="form-control" id="pass_word" required>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" name="button" id="login_button" class="btn btn-primary">Log in</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+  <script>
+    $(function(){
+     $('#login_button').click(function(e){
+
+      var valid = this.form.checkValidity();
+
+      if(valid){
+        var user_name = $('#user_name').val();
+        var pass_word = $('#pass_word').val();
+      }
+
+      e.preventDefault();
+
+      $.ajax({
+        type: 'POST',
+        url: 'jslogin.php',
+        data: {user_name: user_name, pass_word: pass_word},
+        success: function(data){
+          alert(data);
+          if($.trim(data) === "Welcome back!"){
+            setTimeout('window.location.href = "index.php"', 500)
+          }
+        },
+        error: function(data){
+          alert('there were errors while doing the opration.');
+        }
+      });
+
+    });
+   });
+ </script>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+require_once('config.php');
+
+if(isset($_POST['user_name'])){
+	$username=$_POST['user_name'];
+}else{
+	echo 'Username is not set';
+}
+
+if(isset($_POST['pass_word'])){
+	$password=$_POST['pass_word'];
+}else{
+	echo 'Password is not set';
+}
+
+$sql = "SELECT * From user_account WHERE user_name='$username' AND password='$password'";
+$stmt = mysqli_prepare($db_connection, $sql) or die(mysqli_error($db_connection));
+mysqli_stmt_execute($stmt);
+mysqli_stmt_store_result($stmt);
+if(mysqli_stmt_num_rows($stmt)>0)
+{
+	printf("Number of rows: %d.\n", mysqli_stmt_num_rows($stmt));
+	echo 'success! found something';
+}
+else
+	echo 'Your Username or Password is wrong!';
+mysqli_stmt_close($stmt);
+?>
