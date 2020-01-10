e18c882d925635eb5907f545c2223bca0462cf0b
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2020-01-10 13:50:53 -0800

    Naming fixing

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="css/style.css">
+  
 
   <title>Fancy Web</title>
 </head>
@@ -259,6 +260,6 @@
     });
    });
  </script>
- <script src="js/main.js"></script>
+ <script src="js/split.js"></script>
 </body>
 </html>
