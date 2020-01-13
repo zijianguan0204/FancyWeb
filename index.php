88e5e7061bce0975780287af3d4ab466da968434
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2020-01-13 14:22:33 -0800

    hidden navbar
    hidden navbar

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ require_once('config.php');
 
 <body>
   <!-- Nav Bar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
     <a class="navbar-brand" href="#">FancyWeb</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
